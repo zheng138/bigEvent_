74233d0fa9977c5eab21392aea75388c7b34ce8d
tingting <1479660675@qq.com>
2020-12-08 09:09:32 +0800

    提交了user

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,29 +2,17 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-    console.log(options.url);
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
 
-
-    // 统一为有权限的接口,设置headers请求头
-    // indexOf方法,查找字符串  'http://ajax.frontend.itheima.net/my/userinfo" 里面是否包含"/my/"
-    if (options.url.indexOf("/my/") !== -1) {
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
         options.headers = {
-            Authorization: localStorage.getItem("token") || ""
+            Authorization: localStorage.getItem('token') || ''
         }
     }
-    // // 全局挂载complete回调函数
-    // options.complete = function(res) {
-    //     // console.log(res);
-    //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败") {
-    //         // 1强制清空token
-    //         localStorage.removeItem('token')
-    //             // 2. 重新跳转到登录页面
-    //         location.href = '/login.html'
-    //     }
-    // }
-    // 不论成功还是失败，最终都会调用 complete 回调函数
+
+    // 全局统一挂载 complete 回调函数
     options.complete = function(res) {
         // console.log('执行了 complete 回调：')
         // console.log(res)
@@ -36,5 +24,4 @@ $.ajaxPrefilter(function(options) {
             location.href = '/login.html'
         }
     }
-
 })
